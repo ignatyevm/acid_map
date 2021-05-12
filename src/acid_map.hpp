259a728b4facc2be75d13e2172d497d6c45b247d
polyndrom <polyndromdev@gmail.com>
2021-05-12 11:56:38 +1000

    fixes

--- src/acid_map.hpp
+++ src/acid_map.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <tuple>
 #include <ostream>
 
@@ -30,7 +32,7 @@ public:
     using iterator = avl_tree_iterator;
     acid_map(const allocator_type& allocator = allocator_type()) : node_allocator_(allocator) {}
     template <class K>
-    iterator find(const K& key) const {
+    iterator find(const K& key) {
         auto [parent, node] = find_node(root_, key);
         if (node == nullptr) {
             return end();
@@ -57,7 +59,12 @@ public:
     }
     template <class K>
     bool contains(const K& key) const {
-        return find(key) != end();
+        return count(key) == 1;
+    }
+    template <class K>
+    size_type count(const K& key) const {
+        auto [parent, node] = find_node(root_, key);
+        return static_cast<size_type>(node != nullptr);
     }
     template <class V>
     std::pair<iterator, bool> insert(V&& value) {
@@ -123,6 +130,8 @@ public:
     }
     void clear() {
         destroy_subtree(root_);
+        size_ = 0;
+        root_ = nullptr;
     }
     ~acid_map() {
         clear();
@@ -159,7 +168,7 @@ private:
             return node_->value_;
         }
         value_type* operator->() {
-            return node_->value_;
+            return &node_->value_;
         }
         bool operator==(iterator other) {
             return node_ == other.node_;
@@ -439,72 +448,5 @@ private:
     key_compare comparator_;
     mutable node_allocator_type node_allocator_;
 };
-
-template <class Tree>
-bool verify_tree(const Tree& tree, std::ostream& fails_ostream = std::cout);
-
-template <class Tree>
-class tree_verifier {
-public:
-    using node_ptr = typename Tree::node_ptr;
-    tree_verifier(const Tree& tree, std::ostream& fails_ostream) : tree_(tree), fails_ostream_(fails_ostream) {}
-    bool verify() {
-        return verify_node(tree_.root_);
-    }
-    int deep_height(node_ptr node) {
-        if (node == nullptr) {
-            return -1;
-        }
-        return std::max(deep_height(node->left_), deep_height(node->right_)) + 1;
-    }
-    bool verify_node(node_ptr node) {
-        if (node == nullptr) {
-            return true;
-        }
-        node_ptr parent = node->parent_;
-        node_ptr left = node->left_;
-        node_ptr right = node->right_;
-        if (parent != nullptr) {
-            if (tree_.is_left_child(node) && parent->left_ != node) {
-                fails_ostream_ << "parent left node: " << parent->value_.first << " "
-                               << (parent->left_ == nullptr ? -1 : parent->left_->value_.first) << " "
-                               << node->value_.first << std::endl;
-                return false;
-            }
-            if (tree_.is_right_child(node) && parent->right_ != node) {
-                fails_ostream_ << "parent right node: " << parent->value_.first << " "
-                               << (parent->right_ == nullptr ? -1 : parent->right_->value_.first) << " "
-                               << node->value_.first << std::endl;
-                return false;
-            }
-        }
-        if (left != nullptr && left->parent_ != node) {
-            fails_ostream_ << "left left->parent node: " << left->value_.first << " " << left->parent_->value_.first
-                           << " " << node->value_.first << std::endl;
-            return false;
-        }
-        if (right != nullptr && right->parent_ != node) {
-            fails_ostream_ << "right right->parent node: " << right->value_.first << " " << right->parent_->value_.first
-                           << " " << node->value_.first << std::endl;
-            return false;
-        }
-        int lheight = deep_height(node->left_);
-        int rheight = deep_height(node->right_);
-        int bf = lheight - rheight;
-        if (bf > 1 || bf < -1) {
-            fails_ostream_ << "node lh rh " << node->value_.first << " " << lheight << " " << rheight << std::endl;
-            return false;
-        }
-        return verify_node(left) && verify_node(right);
-    }
-    const Tree& tree_;
-    std::ostream& fails_ostream_;
-};
-
-template <class Tree>
-bool verify_tree(const Tree& tree, std::ostream& fails_ostream) {
-    tree_verifier verifier(tree, fails_ostream);
-    return verifier.verify();
-}
 
 } // polyndrom
