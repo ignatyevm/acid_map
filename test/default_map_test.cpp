80ca55cdc74a1d606f52f7bef7ce4df21c3e95be
polyndrom <polyndromdev@gmail.com>
2021-05-12 11:57:25 +1000

    add simple test

--- test/default_map_test.cpp
+++ test/default_map_test.cpp
@@ -1,14 +1,32 @@
-#include "gtest/gtest.h"
-
 #include "acid_map.hpp"
-#include "pure_map_tester.h"
+#include "default_map_tester.h"
 #include "tree_verifier.hpp"
 
-TEST(PureMap, EmptyMap) {
-    polyndrom::acid_map<int, int> map;
-    EXPECT_EQ(map.size(), 0);
-    EXPECT_TRUE(map.empty());
-    EXPECT_TRUE(verify_tree(map));
+#include <vector>
+#include <random>
+
+#include "gtest/gtest.h"
+
+std::vector<int> generate_values(int n, int start, int end) {
+    std::vector<int> values;
+    values.reserve(n);
+    std::random_device device;
+    std::uniform_int_distribution<int> distribution(start, end);
+    for (int i = 0; i < n; i++) {
+        values.push_back(distribution(device));
+    }
+    return values;
+}
+
+TEST(DefaultMap, test1) {
+    polyndrom::default_map_tester<int, int> default_map_tester;
+    std::vector<int> values = generate_values(100000, 0, 1000000);
+    for (int v : values) {
+        default_map_tester.insert(std::make_pair(v, v));
+    }
+    /*int* a = new int(5);
+    a = new int(10);*/
+    EXPECT_TRUE(polyndrom::verify_tree(default_map_tester.testable_map()));
 }
 
 
