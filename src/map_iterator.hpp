ef2ae4f223a2fb12b4b78290283a9eb4238ed65e
polyndrom <polyndromdev@gmail.com>
2021-06-07 19:57:54 +1000

    some changes

--- src/map_iterator.hpp
+++ src/map_iterator.hpp
@@ -39,9 +39,6 @@ public:
         --*this;
         return other;
     }
-    bool is_deleted() const {
-        return node_->is_deleted_;
-    }
     value_type& operator*() {
         return node_->value_;
     }
